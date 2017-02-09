1b062a8a9a3e0575b33eb62050d86afe83f95f76
jhw-at-prosperworks-com <jhw@prosperworks.com>
2017-02-09 14:21:11 -0800

    INCIDENT_EXPIRATION_SECONDS and NOTIFICATION_EXPIRATION_SECONDS

--- graphitepager/redis_storage.py
+++ graphitepager/redis_storage.py
@@ -3,8 +3,9 @@ import json
 
 class RedisStorage(object):
 
-    def __init__(self, redis_lib, url):
+    def __init__(self, redis_lib, url, config):
         self._client = redis_lib.from_url(url)
+        self._config = config
 
     def get_incident_key_for_alert_key(self, alert):
         key = self._redis_key_from_alert_key(alert)
@@ -15,8 +16,9 @@ class RedisStorage(object):
     def set_incident_key_for_alert_key(self, alert, ik):
         data = {'incident': ik}
         key = self._redis_key_from_alert_key(alert)
+        exp = int(self._config.get('INCIDENT_EXPIRATION_SECONDS',3600))
         self._client.set(key, json.dumps(data))
-        self._client.expire(key, 3600)
+        self._client.expire(key, exp)
 
     def remove_incident_for_alert_key(self, alert):
         key = self._redis_key_from_alert_key(alert)
@@ -24,8 +26,9 @@ class RedisStorage(object):
 
     def set_lock_for_domain_and_key(self, domain, key):
         key = 'LOCK-{0}-{1}'.format(domain, key)
+        exp = int(self._config.get('NOTIFICATION_EXPIRATION_SECONDS',300))
         self._client.set(key, True)
-        self._client.expire(key, 300)
+        self._client.expire(key, exp)
 
     def remove_lock_for_domain_and_key(self, domain, key):
         key = 'LOCK-{0}-{1}'.format(domain, key)
