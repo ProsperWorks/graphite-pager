f725f7ab3300d9b48acc446ac6c7458ea629bf67
Ryan Payton <>
2020-06-02 15:19:08 -0700

    no longer converting description to str

--- graphitepager/notifiers/pagerduty_notifier.py
+++ graphitepager/notifiers/pagerduty_notifier.py
@@ -46,7 +46,7 @@ class PagerdutyNotifier(BaseNotifier):
         # in the next business hours".
         #
         if level == Level.CRITICAL:
-            description = str(description)
+            description = bytes(description)
             incident_key = self._client.trigger(
                 incident_key=incident_key,
                 description=description
