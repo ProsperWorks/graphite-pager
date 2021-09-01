3fdc35c079c62422c1ae1b712b2155d60efc50e7
Fabien Pelletier <fabien@fabienpelletier.com>
2021-09-01 13:27:59 -0400

    tentatively remove print debugging

--- graphitepager/redis_storage.py
+++ graphitepager/redis_storage.py
@@ -45,12 +45,9 @@ class RedisStorage(object):
         return '{0}-incident-key'.format(alert_key)
 
     def increment_no_data_count_for_alert(self, alert):
-        print(alert)
         key = '{0}-no-data-counter'.format(alert)
         exp = int(self._config.get('NO_DATA_COUNTER_EXPIRATION_SECONDS',300))
         current = self._client.get(key)
-        print(current)
-        print(self._client.keys('*'))
         counter = (self._client.get(key) or 0) + 1
         if self._config.get('NO_DATA_COUNTER_VERBOSE',None):
             print("incrementing 'NO DATA' counter for {0} to {1} with TTL {2}".format(key, counter, exp))
