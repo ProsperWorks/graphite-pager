8d9cd65a89e7d823af5bc52e5684dd9c78e90207
Danny Tom <danny@copper.com>
2025-01-01 05:36:33 -0800

    remove a single slash from the url after https://

--- graphitepager/description.py
+++ graphitepager/description.py
@@ -144,7 +144,7 @@ class Description(object):
         while os.path.dirname(path) != '/':
             path = os.path.dirname(path)
 
-        grafana_url = 'https://' \
+        grafana_url = 'https:/' \
                       + path \
                       + '.hostedgraphite.com/d/' \
                       + 'bd9883de-1f24-47d8-ba91-5672ed8ff4b9/' \
